98c7575796aa3c506930ba9e6176407bcf00764f
Asaf53 <asafrushiti3@gmail.com>
2024-12-05 20:13:02 +0100

    new

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -24,59 +24,102 @@ if (!isset($_SESSION['user_id'])) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard Admin</title>
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600">
-    <link rel="stylesheet" href="css/fontawesome.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/tooplate.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/boxicons.min.css">
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.22.4/dist/bootstrap-table.min.css">
 </head>
 
 
-<body id="reportsPage">
+<body>
     <?php if (isset($_SESSION['role']) && ($_SESSION['role'] !== 'admin')) {
         header('Location: error.html');
         die();
     } else { ?>
-        <div class="" id="home">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <nav class="navbar navbar-expand-xl navbar-light bg-light">
-                            <a class="navbar-brand" href="index.php">
-                                <h1 class="tm-site-title mb-0">Dashboard</h1>
-                            </a>
-                            <button class="navbar-toggler ml-auto mr-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container-fluid py-2">
+            <a class="navbar-brand text-light" href="index.php">Dashboard</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-light <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Goods</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Documents</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Expenses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Reports</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Settings</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="btn btn-outline-light d-flex align-items-center" href="?csrf_token=<?php echo $_SESSION['csrf_token']; ?>">
+                            <i class='bx bx-user me-1 h4 p-0 m-0'></i>
+                            <span>Logout</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- <div>
+        <div class="row">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-xl navbar-light bg-light">
+                    <a class="navbar-brand" href="index.php">
+                        <h1 class="tm-site-title mb-0">Dashboard</h1>
+                    </a>
+                    <button class="navbar-toggler ml-auto mr-0" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav mx-auto">
-                                    <li class="nav-item">
-                                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>" href="index.php" href="index.php">Dashboard</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'product.php' ? 'active' : ''; ?>" href="product.php" href="cars.php">Product</a>
-                                    </li>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item">
+                                <a class="nav-link <?php #echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>"
+                                    href="index.php" href="index.php">Dashboard</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link <?php #echo basename($_SERVER['PHP_SELF']) == 'product.php' ? 'active' : ''; ?>"
+                                    href="product.php" href="cars.php">Product</a>
+                            </li>
 
-                                    <li class="nav-item">
-                                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'order.php' ? 'active' : ''; ?>" href="order.php" href="order.php">Order</a>
-                                    </li>
-                                    
-                                    <li class="nav-item">
-                                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'accounts.php' ? 'active' : ''; ?>" href="accounts.php" href="accounts.php">Accounts</a>
-                                    </li>
-                                </ul>
-                                <ul class="navbar-nav">
-                                    <li class="nav-item">
-                                        <a class="nav-link d-flex" href="?csrf_token=<?php echo $_SESSION['csrf_token']; ?>">
-                                            <i class="far fa-user mr-2 tm-logout-icon"></i>
-                                            <span>Logout</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
+                            <li class="nav-item">
+                                <a class="nav-link <?php #echo basename($_SERVER['PHP_SELF']) == 'order.php' ? 'active' : ''; ?>"
+                                    href="order.php" href="order.php">Order</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link <?php #echo basename($_SERVER['PHP_SELF']) == 'accounts.php' ? 'active' : ''; ?>"
+                                    href="accounts.php" href="accounts.php">Accounts</a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link d-flex" href="?csrf_token=<?php #echo $_SESSION['csrf_token']; ?>">
+                                    <i class="far fa-user mr-2 tm-logout-icon"></i>
+                                    <span>Logout</span>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                </div>
-            <?php }; ?>
+                </nav>
+            </div>
+        </div> -->
+        <?php }; ?>
--- admin/index.php
+++ admin/index.php
@@ -1,21 +1,109 @@
-<?php include_once('includes/header.php');
-?>
-<div class="row tm-content-row tm-mt-big mt-3">
-    <div class="col-xl-12 col-lg-12 tm-md-12 tm-sm-12 tm-col">
-        <div class="bg-white tm-block h-100">
-            <div class="row align-items-center justify-content-between">
-                <div class="col-md-8 col-4">
-                    <h2 class="tm-block-title d-inline-block mb-0">Products</h2>
+<?php include_once('includes/header.php'); ?>
+
+<div class="container-fluid">
+    <div class="row mt-4 col-12 justify-content-center p-0 m-0">
+        <div class="col-12 col-md-4 col-xl-4 m-2">
+            <a href="#" class="text-decoration-none">
+                <div class="card bg-primary">
+                    <div class="card-body text-center">
+                        <h5 class="card-title"><img src="./assets/icons/package.svg" alt=""></h5>
+                        <h5 class="card-title text-white">Goods</h5>
+                        <p class="card-text text-white">776 / 6,340.63</p>
+                    </div>
                 </div>
-                <div class="col-md-4 col-8 d-flex justify-content-end">
-                    <a href="add-order.php" class="btn btn-small btn-primary">Add New Order</a>
+            </a>
+        </div>
+        <div class="col-12 col-md-4 col-xl-4 m-2">
+            <a href="#" class="text-decoration-none">
+                <div class="card bg-primary">
+                    <div class="card-body text-center">
+                        <h5 class="card-title"><img src="./assets/icons/documents.svg" alt=""></h5>
+                        <h5 class="card-title text-white">Documents</h5>
+                        <p class="card-text text-white">776</p>
+                    </div>
                 </div>
-            </div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4 col-xl-4 m-2">
+            <a href="#" class="text-decoration-none">
+                <div class="card bg-primary">
+                    <div class="card-body text-center">
+                        <h5 class="card-title"><img src="./assets/icons/package-plus.svg" alt=""></h5>
+                        <!-- <h5 class="card-title text-white">Goods</h5>
+                        <p class="card-text text-white">776 / 6,340.63</p> -->
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4 col-xl-4 m-2">
+            <a href="#" class="text-decoration-none">
+                <div class="card bg-primary">
+                    <div class="card-body text-center">
+                        <h5 class="card-title"><img src="./assets/icons/package-minus.svg" alt=""></h5>
+                        <!-- <h5 class="card-title text-white">Goods</h5>
+                        <p class="card-text text-white">776 / 6,340.63</p> -->
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4 col-xl-4 m-2">
+            <a href="#" class="text-decoration-none">
+                <div class="card bg-primary">
+                    <div class="card-body text-center">
+                        <h5 class="card-title"><img src="./assets/icons/reports.svg" alt=""></h5>
+                        <h5 class="card-title text-white">Reports</h5>
+                        <!-- <p class="card-text text-white">776 / 6,340.63</p> -->
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
 </div>
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+
+
+<!-- <div class="container-fluid mt-4">
+        <div class="text-start">
+            <h3 class="fw-bold">Recent Documents</h3>
+        </div>
+        <div class="col-12">
+            <div class="card bg-white border-0 rounded-0 border-bottom">
+                <div class="card-body d-flex justify-content-between">
+                    <div class="d-flex">
+                        <span class="w-10 h-100 bg-danger"></span>
+                        <div class="ms-2">
+                            <a href="#" class="link-primary text-decoration-none card-title">
+                                <h4>Incoming №51</h4>
+                            </a>
+                            <p class="card-text text-secondary mb-0 fw-medium">from 10/02/21</p>
+                            <p class="card-text text-secondary fw-medium">Ironman Factory</p>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <h3 class="text-secondary">98</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-white border-0 rounded-0 border-bottom">
+                <div class="card-body d-flex justify-content-between">
+                    <div class="d-flex">
+                        <span class="w-10 h-100 bg-success"></span>
+                        <div class="ms-2">
+                            <a href="#" class="link-primary text-decoration-none card-title">
+                                <h4>Incoming №51</h4>
+                            </a>
+                            <p class="card-text text-secondary mb-0 fw-medium">from 10/02/21</p>
+                            <p class="card-text text-secondary fw-medium">Ironman Factory</p>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <h3 class="text-secondary">98</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+<script src="assets/js/jquery-3.3.1.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
 <script src="https://unpkg.com/bootstrap-table@1.22.4/dist/bootstrap-table.min.js"></script>
 </body>
 
