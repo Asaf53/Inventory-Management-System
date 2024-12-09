c51d8c83143025f803a9f29429ca254db6af627a
Asaf53 <asafrushiti3@gmail.com>
2024-12-09 23:01:13 +0100

    new

--- titan-cink.sql
+++ titan-cink.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 30, 2024 at 06:26 PM
+-- Generation Time: Dec 09, 2024 at 10:00 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.3.12
 
@@ -18,56 +18,106 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `titan-cink`
+-- Database: `test_titan_cink`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Table structure for table `categories`
 --
 
-CREATE TABLE `orders` (
+CREATE TABLE `categories` (
   `id` int NOT NULL,
-  `product_id` int NOT NULL,
-  `client_name` varchar(50) NOT NULL,
-  `client_phone` varchar(20) NOT NULL,
-  `order_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `qty` int NOT NULL,
-  `price` float NOT NULL
+  `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `orders`
+-- Dumping data for table `categories`
 --
 
-INSERT INTO `orders` (`id`, `product_id`, `client_name`, `client_phone`, `order_date`, `qty`, `price`) VALUES
-(1, 1, 'Asaf', '111-222-333', '2024-10-01 00:00:00', 20, 500);
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Sandwich Panel 5cm'),
+(2, 'Sandwich Panel 3cm'),
+(3, 'Sandwich Panel Antracid 5cm'),
+(4, 'Sandwich Panel Antracid 3cm'),
+(5, 'Sandwich Panel Fasad 5cm'),
+(6, 'Sandwich Panel Fasad 3cm'),
+(7, 'Sandwich Panel Fasad Antracid 5cm'),
+(13, 'Sandwich Panel Fasad Antracid 3cm');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product`
+-- Table structure for table `inventorysummary`
 --
 
-CREATE TABLE `product` (
+CREATE TABLE `inventorysummary` (
+  `product_id` int NOT NULL,
+  `current_qty` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `inventorysummary`
+--
+
+INSERT INTO `inventorysummary` (`product_id`, `current_qty`) VALUES
+(4, 40);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
   `id` int NOT NULL,
   `name` varchar(100) NOT NULL,
-  `type` enum('Roof Panel','Sandwich Panel') NOT NULL,
   `length` int NOT NULL,
-  `height` int NOT NULL,
-  `width` int NOT NULL,
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `price` int NOT NULL,
-  `qty` int NOT NULL
+  `category_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `product`
+-- Dumping data for table `products`
 --
 
-INSERT INTO `product` (`id`, `name`, `type`, `length`, `height`, `width`, `description`, `price`, `qty`) VALUES
-(1, 'Sandwich Panel 1', 'Sandwich Panel', 15, 1, 5, NULL, 150, 50);
+INSERT INTO `products` (`id`, `name`, `length`, `category_id`) VALUES
+(4, 'testestest', 150, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `sale_date` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `type` enum('incoming','outgoing') NOT NULL,
+  `quantity` int NOT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `product_id`, `type`, `quantity`, `date`) VALUES
+(13, 4, 'incoming', 15, '2024-12-09 19:11:42'),
+(14, 4, 'outgoing', 30, '2024-12-09 19:12:10');
 
 -- --------------------------------------------------------
 
@@ -81,32 +131,54 @@ CREATE TABLE `user` (
   `email` varchar(50) NOT NULL,
   `password` varchar(65) NOT NULL,
   `phone` varchar(20) NOT NULL,
-  `role` enum('client','admin') NOT NULL
+  `role` enum('client','admin') NOT NULL,
+  `login_token` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `fullname`, `email`, `password`, `phone`, `role`) VALUES
-(6, 'Asaf Rushiti', 'asaf222@gmail.com', '$2y$10$HcMfr8Uit2sDjSbJgP5pHOjld2wqNbunDOXw2r9LlFBx/XDZq6T36', '+389 70832727', 'admin');
+INSERT INTO `user` (`id`, `fullname`, `email`, `password`, `phone`, `role`, `login_token`) VALUES
+(6, 'Asaf Rushiti', 'asaf222@gmail.com', '$2y$10$HcMfr8Uit2sDjSbJgP5pHOjld2wqNbunDOXw2r9LlFBx/XDZq6T36', '+389 70832727', 'admin', ''),
+(7, 'Asaf Rushiti', 'asaf@gmail.com', '$2y$10$T9lfWvVA.BEsf/.U.NBWDOQtuiaSCtj8/dmlSgG9Ef5BqRusi.kO2', '000111222', 'admin', '$2y$10$kSiGpUwhUlYTZHscGj/ppOgsFplqHSqk/S4oB9HTrChjdjrw/TQQy');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `orders`
+-- Indexes for table `categories`
 --
-ALTER TABLE `orders`
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `inventorysummary`
+--
+ALTER TABLE `inventorysummary`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
   ADD PRIMARY KEY (`id`),
   ADD KEY `product_id` (`product_id`);
 
 --
--- Indexes for table `product`
+-- Indexes for table `transactions`
 --
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `user`
@@ -120,32 +192,62 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `orders`
+-- AUTO_INCREMENT for table `categories`
 --
-ALTER TABLE `orders`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT for table `product`
+-- AUTO_INCREMENT for table `products`
 --
-ALTER TABLE `product`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `products`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `orders`
+-- Constraints for table `inventorysummary`
 --
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `inventorysummary`
+  ADD CONSTRAINT `inventorysummary_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
