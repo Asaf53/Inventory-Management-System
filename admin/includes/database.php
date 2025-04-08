d437f9f8939978ce76e068e741a1d968204930a6
Asaf53 <asafrushiti3@gmail.com>
2025-04-08 22:33:53 +0200

    new

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,7 +1,8 @@
 <?php
 $host = '127.0.0.1';
-$db = 'inventory-management-system-db';
-$dsn = "mysql:host=$host;dbname=$db;";
+$db = 'inventory-management-system';
+$port = '3309';
+$dsn = "mysql:host=$host;dbname=$db;port=$port";
 $username = 'root';
 $password = '';
 $pdo = new PDO($dsn, $username, $password);
